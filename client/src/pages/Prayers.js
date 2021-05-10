7eed65018d6ac14a25aaeedac1bef2f86eed9011
Kenneth Keller <kenkanifffromct21@gmail.com>
2021-05-10 19:48:52 -0400

    updated dependencies

--- client/src/pages/Prayers.js
+++ client/src/pages/Prayers.js
@@ -5,7 +5,7 @@ import API from "../utils/API";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
-import { Input, TextArea, FormBtn, Select } from "../components/Form";
+import { Input, TextArea, FormBtn } from "../components/Form";
 
 function Prayers() {
   // Setting our component's initial state
